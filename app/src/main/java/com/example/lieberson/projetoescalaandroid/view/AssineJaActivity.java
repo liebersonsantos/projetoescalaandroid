99d8022e2553d715297562c5ecdf1a6b65d9216d
liebersonsantos <liebersonsantos.one@gmail.com>
2018-05-14 17:30:15 -0300

    Criacao de novas classes e Xml

---
+++ app/src/main/java/com/example/lieberson/projetoescalaandroid/view/AssineJaActivity.java
@@ -0,0 +1,15 @@
+package com.example.lieberson.projetoescalaandroid.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lieberson.projetoescalaandroid.R;
+
+public class AssineJaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assine_ja);
+    }
+}
--- app/src/main/java/com/example/lieberson/projetoescalaandroid/view/fragment/LancamentosFragment.java
+++ app/src/main/java/com/example/lieberson/projetoescalaandroid/view/fragment/LancamentosFragment.java
@@ -1,18 +1,24 @@
 package com.example.lieberson.projetoescalaandroid.view.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.lieberson.projetoescalaandroid.R;
+import com.example.lieberson.projetoescalaandroid.view.ApresentacaoActivity;
+import com.example.lieberson.projetoescalaandroid.view.ApresentacaoRevistaActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LancamentosFragment extends Fragment {
+
+    private ImageView imagemCorpo;
 
 
     public LancamentosFragment() {
@@ -24,7 +30,23 @@ public class LancamentosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lancamentos, container, false);
+        View view =  inflater.inflate(R.layout.fragment_lancamentos, container, false);
+
+        imagemCorpo = view.findViewById(R.id.imageView15);
+
+        imagemCorpo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), ApresentacaoRevistaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        return view;
+
     }
 
 }
